e737387daf5f89c9de945ec791dd43a601249cef
handoiditu1508 <handoiditu1508@gmail.com>
2020-05-28 20:55:21 +0700

    20:55 2020-05-28

--- test.py
+++ test.py
@@ -6,12 +6,20 @@ from scipy.stats import norm
 from scipy.stats import expon
 from scipy.stats import binom
 from pylab import *
+from sklearn.metrics import r2_score
+import pandas as pd
+import statsmodels.api as sm
 
 clear = lambda: os.system('cls')
 clear()
 
-pageSpeeds = np.random.normal(3.0,1.0,100)
-purchaseAmount = 100 - (pageSpeeds + np.random.normal(0,0.1,100))*3
-scatter(pageSpeeds, purchaseAmount)
-plt.plot(pageSpeeds, purchaseAmount,".")
-plt.show()
+df = pd.read_excel("http://cdn.sundog-soft.com/Udemy/DataScience/cars.xls")
+df["Model_ord"] = pd.Categorical(df.Model).codes
+x=df[["Mileage","Model_ord","Doors"]]
+y=df[["Price"]]
+
+X1=sm.add_constant(x)
+est=sm.OLS(y,X1).fit()
+
+#print(est.summary())
+print(y.groupby(df.Doors).mean())
