3dc9ddff51ae813e4b2965780d10fb172a24e7a5
handoiditu1508 <handoiditu1508@gmail.com>
2020-06-04 23:42:57 +0700

    23:43 2020-06-04

--- test.py
+++ test.py
@@ -17,12 +17,7 @@ from sklearn.preprocessing import scale
 clear = lambda: os.system('cls')
 clear()
 
-np.random.seed(2)
-a = []
-for i in range(5):
-	a.append([int(np.random.normal(500, 500)), int(np.random.normal(500, 500))])
-a=array(a)
-b = scale(a)
-plt.scatter(a[:,0],a[:,1],c="r")
-#plt.scatter(b[:,0],b[:,1],c="b")
+x=[74.1, 74.77, 73.94, 73.61, 73.4]
+plt.scatter(x,[0]*len(x),c="b")
+plt.scatter(np.mean(x),0,c="r")
 plt.show()
